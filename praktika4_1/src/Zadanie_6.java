c505c90459e92b6614a52d12b09a787a7ac82481
elemanjan <namele93@gmail.com>
2020-11-26 17:40:05 +0600

    zadanie6

--- praktika4_1/src/Zadanie_6.java
+++ praktika4_1/src/Zadanie_6.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class Zadanie_6 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[] list1 = new int[5];
-        int[] list2 = new int[5];
-        int[] mergedList = new int[list1.length+list2.length];
-        int counter = 0;
+        int[] list1 = new int[3];
+        int[] list2 = new int[3];
+//        int counter = 0;
+        int flag = 0;
         System.out.println("Enter the values 1st list");
         for (int i = 0; i < list1.length ; i++) {
             list1[i] = scan.nextInt();
@@ -18,20 +18,16 @@ public class Zadanie_6 {
         for (int i = 0; i < list2.length; i++){
             list2[i] = scan.nextInt();
         }
-
-        for (int i = 0; i < list1.length ; i++) {
-            for (int j = 0; j < list2.length; j++) {
-                if (list1[i] == list2[j]){
-                    mergedList[counter] = list1[i];
-                    counter++;
-                }
-                else {
-                    mergedList[counter] = list1[i];
-                    counter++;
-                    mergedList[counter] = list2[j];
+        for (int i = 0; i < list1.length; i++) {
+            System.out.print(list1[i] + " ");
+        }
+        for (int i = 0; i < list2.length ; i++) {
+            for (int j = 0; j < list1.length; j++) {
+                if (list2[i] != list1[j]){
+                    flag = i;
                 }
             }
+            System.out.print(list2[flag] + " ");
         }
-        System.out.println(Arrays.toString(mergedList));
     }
 }
