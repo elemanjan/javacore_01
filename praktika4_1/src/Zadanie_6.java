befb847f8db3b2c6a19e3619592e15cdd321e12b
elemanjan <namele93@gmail.com>
2020-11-27 13:20:27 +0600

    zadanie 6 done

--- praktika4_1/src/Zadanie_6.java
+++ praktika4_1/src/Zadanie_6.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class Zadanie_6 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[] list1 = new int[3];
-        int[] list2 = new int[3];
-//        int counter = 0;
+        int[] list1 = new int[10];
+        int[] list2 = new int[10];
         int flag = 0;
+        int counter = 0;
         System.out.println("Enter the values 1st list");
         for (int i = 0; i < list1.length ; i++) {
             list1[i] = scan.nextInt();
@@ -22,12 +22,18 @@ public class Zadanie_6 {
             System.out.print(list1[i] + " ");
         }
         for (int i = 0; i < list2.length ; i++) {
+            counter = 0;
             for (int j = 0; j < list1.length; j++) {
-                if (list2[i] != list1[j]){
+                if (list2[i] == list1[j]){
+                    counter++;
+                }
+                else{
                     flag = i;
                 }
             }
-            System.out.print(list2[flag] + " ");
+            if (counter == 0){
+                System.out.print(list2[flag] + " ");
+            }
         }
     }
 }
