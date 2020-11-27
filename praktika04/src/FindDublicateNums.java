abb16877009891b7fb12bbcc1a0f0a5bc43f91b3
elemanjan <namele93@gmail.com>
2020-11-27 17:24:42 +0600

    praktika4 zadanie 1 done

--- praktika04/src/FindDublicateNums.java
+++ praktika04/src/FindDublicateNums.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
+// Praktika4.Zadanie 1
+// Считайте с консоли лист чисел, размером 10, и выведите уникальные числа, то есть дубликаты удалить
 
 public class FindDublicateNums {
     public static void main(String[] args) {
@@ -7,17 +9,20 @@ public class FindDublicateNums {
         int[] list = new int[5];
         int[] uniqList = new int[list.length];
         for (int i = 0; i < list.length; i++){
+            System.out.println("Enter the number ↓");
             list[i] = sc.nextInt();
         }
-
+        int c = 0;
         for (int i = 0; i < list.length; i++){
+            int counter = 0;
             for (int j = i + 1; j < list.length; j++){
                 if (list[i] == list[j]){
-
+                    counter++;
                 }
-                else{
-                    uniqList[i] = list[i];
-                }
+            }
+            if (counter == 0){
+                uniqList[c] = list[i];
+                c++;
             }
         }
         System.out.println(Arrays.toString(uniqList));
--- praktika04/src/SumOfEveryNums2.java
+++ praktika04/src/SumOfEveryNums2.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class SumOfEveryNums2 {
+    public static void main(String[] args) {
+        System.out.print("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        int nums = sc.nextInt();
+        int sum = 0;
+        int i = 0;
+        /*String[] numArr = nums.split("");
+        for (int i = 0; i < numArr.length; i++){
+            sum += Integer.parseInt(numArr[i]);
+        }*/
+        for (i = nums; i > 0; i /= 10){
+            sum += i % 10;
+        }
+        System.out.println("The digit sum is: " + sum);
+    }
 }
